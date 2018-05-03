8ec3a5c66985b10bc5947bc8d4f6ea5fc6758005
William Robertson <williamp.robertson@gmail.com>
2018-05-03 01:51:22 -0700

    firebase storage added, style updates

---
+++ landing/js/main.js
@@ -0,0 +1,49 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCyYe6-iGDVsYhCm2p0PnSIHubaBw7uHDQ",
+  authDomain: "ish-contact.firebaseapp.com",
+  databaseURL: "https://ish-contact.firebaseio.com",
+  projectId: "ish-contact",
+  storageBucket: "",
+  messagingSenderId: "636022481161"
+};
+firebase.initializeApp(config);
+
+// reference messages collection
+var messagesRef = firebase.database().ref("messages");
+
+// listen for form submit
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+// submit contact form
+function submitForm(e) {
+  e.preventDefault();
+
+  // get values
+  var choice = getInputVal("choice");
+  var email = getInputVal("email");
+  // save message
+  saveMessage(choice, email);
+  // show alert
+  document.querySelector(".alert").style.display = "block";
+  // hide alert after 2 seconds
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+  // clear form
+  document.getElementById("contactForm").reset();
+}
+
+// function to get form values
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+// save message to firebase
+function saveMessage(choice, email) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    choice: choice,
+    email: email
+  });
+}
