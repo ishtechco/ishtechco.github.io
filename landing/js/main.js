31df136af785f7e8c078589078ce1ebc64ba18ac
William Robertson <williamp.robertson@gmail.com>
2018-05-03 19:16:37 -0700

    added modal with survey link and fixed main section text

--- landing/js/main.js
+++ landing/js/main.js
@@ -47,3 +47,51 @@ function saveMessage(choice, email) {
     email: email
   });
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("subscribe");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Modal form controls
+$(document).ready(function() {
+  //if you wish to keep both the divs hidden by default then dont forget to hide //them
+  $("#help-content").hide();
+  $("#about-content").show();
+
+  $("#about-anchor").click(function() {
+    $("#help-content").hide();
+    $("#about-content").show();
+  });
+
+  $("#help-anchor").click(function() {
+    $("#help-content").show();
+    $("#about-content").hide();
+  });
+
+  $("#cta-button").click(function() {
+    $("#help-content").show();
+    $("#about-content").hide();
+  });
+});
